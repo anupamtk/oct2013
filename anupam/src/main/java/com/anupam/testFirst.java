8421eff1a441fa85fd215e675a52e91413497ce3
anupamtk <anupamtk@yahoo.com>
2013-10-05 17:47:12 +0530

    first commit

---
+++ anupam/src/main/java/com/anupam/testFirst.java
@@ -0,0 +1,56 @@
+package com.anupam;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+@Test
+public class testFirst {
+	
+	@Test(groups = {"new"}, invocationCount=1)
+	public void test1(ITestContext ctx) {
+
+		WebDriver driver = new FirefoxDriver();
+//		WebDriver driver = ChromeDriver();
+
+		// And now use this to visit Google
+		driver.get("http://www.google.com");
+		// Alternatively the same thing can be done like this
+		// driver.navigate().to("http://www.google.com");
+
+		// Find the text input element by its name
+		WebElement element = driver.findElement(By.name("q"));
+
+		// Enter something to search for
+		element.sendKeys("Cheese!");
+
+		// Now submit the form. WebDriver will find the form for us from the
+		// element
+		element.submit();
+
+		// Check the title of the page
+		System.out.println("Page title is: " + driver.getTitle());
+
+		// Google's search is rendered dynamically with JavaScript.
+		// Wait for the page to load, timeout after 10 seconds
+		(new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver d) {
+				return d.getTitle().toLowerCase().startsWith("cheese!");
+			}
+		});
+
+		// Should see: "cheese! - Google Search"
+		System.out.println("Page title is: " + driver.getTitle());
+		
+		System.out.println(ctx.getSuite().getXmlSuite().getParameter("abc"));
+
+		// Close the browser
+		driver.quit();
+	}
+
+}
